fccdfaaa385e72ff273c53698a5155d98ffbbee9
Luis Miguel Morillas <morillas@gmail.com>
2016-06-08 23:03:56 +0200

    Commit inicial

---
+++ pliego/pliego/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PliegoItem(scrapy.Item):
+    # Fields Pliego Items
+    lote = scrapy.Field()
+    url = scrapy.Field()
+    img_url = scrapy.Field()
+    img_desc = scrapy.Field()
+    lote = scrapy.Field()
+    apartado = scrapy.Field()
+    descripcion = scrapy.Field()
+
+
+
---
+++ pliego/pliego/spiders/pliego.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+from pliego.items import PliegoItem
+
+BASE = 'http://subastas.numismaticaycoleccionismo.es/'
+
+
+class PliegoSpider(scrapy.Spider):
+    name = "pliego"
+    allowed_domains = ["subastas.numismaticaycoleccionismo.es"]
+    start_urls = ('http://subastas.numismaticaycoleccionismo.es/index/viewBatch/18193/26a-subasta-online-24-05-16', )
+
+
+    def parse(self, response):
+        next = response.xpath('//a[@title="View Next Lot"]/@href').extract()
+        if next:
+            yield Request( BASE + next[0] )
+
+        item = PliegoItem()
+        item['url'] = response.url
+        item['lote'] = response.xpath('//div[@id="table_bids"]//h1/text()').extract()
+        item['img_url'] = response.xpath('//img[@class="list_logo"]/@src').extract()
+        item['img_desc'] = response.xpath('//img[@class="list_logo"]/@alt').extract()
+
+        _desc = response.xpath('//div[@class="description"]//text()').extract()
+        _desc = [d.strip() for d in _desc if d.strip()]
+        _desc = [d.strip() for d in _desc if d != '.']
+
+        item['lote'] = _desc[0]
+        item['apartado'] = _desc[1]
+        item['descripcion'] = _desc[2]
+
+        yield item
